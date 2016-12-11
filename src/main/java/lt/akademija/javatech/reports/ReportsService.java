30c81825792896229c4d21bc0944a11ca5ec7127
Antanas Bastys <antanas.bastys@tieto.com>
2016-12-11 15:57:07 +0200

    Implemented using single query

--- src/main/java/lt/akademija/javatech/reports/ReportsService.java
+++ src/main/java/lt/akademija/javatech/reports/ReportsService.java
@@ -1,7 +1,6 @@
 package lt.akademija.javatech.reports;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +12,6 @@ public class ReportsService {
     private ReportsRepository repository;
 
     public List<LcdMonitorReport> getLcdMonitorReport() {
-        return repository.getLcdMonitorReport().stream()
-                         .map(p -> {
-                             LcdMonitorReport r = new LcdMonitorReport();
-                             r.title = p.getTitle();
-                             r.count = (long) p.getCarts().size();
-                             return r;
-                         })
-                         .collect(Collectors.toList());
+        return repository.getLcdMonitorReport();
     }
 }

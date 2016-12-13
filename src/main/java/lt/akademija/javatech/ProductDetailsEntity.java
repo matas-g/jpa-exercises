6c02f478142c00cb324b80b07ae8a8a5a578aeeb
matas-g <matas.guzelis@gmail.com>
2016-12-13 02:07:49 +0200

    ex02 - DONE. With comments.

--- src/main/java/lt/akademija/javatech/ProductDetailsEntity.java
+++ src/main/java/lt/akademija/javatech/ProductDetailsEntity.java
@@ -3,6 +3,7 @@
  */
 package lt.akademija.javatech;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,31 +12,45 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class ProductDetailsEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	private String details;
-	
-	// mapped by reikalinga tik dvipusiam (bidirectional) rysiui
-	@OneToOne //(mappedBy = "details")
-	private ProductEntity product;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	// Getters and Setters
-	public Long getId() {
-		return id;
-	}
+    private String details;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    // Bi-directional One to One
+    /*
+     * 1. I vaikine Entity pridedam tevines Entity objekta
+     *    
+     *    !!! Pridejus jo getters ir setters prasideda Stack Overflow !!!
+     *    - turbut to galima isvengt sukurus ProductDetails objektu sukurimo
+     *      metodus Controller/Service/Repository.
+     *      
+     * 2. Pridedam anotacija OneToOne su "mappedBy".
+     * 
+     * mappedBy reikalinga tik dvipusiam (Bi-directional) rysiui.
+     * 
+     * Prie mappedBy turi buti nurodytas tevineje Entity klaseje sukurto
+     * ProductDetails objekto pavadinimas.
+     */
+    @OneToOne(mappedBy = "pDetails", cascade = CascadeType.ALL)
+    private ProductEntity product;
 
-	public String getDetails() {
-		return details;
-	}
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
-	public void setDetails(String details) {
-		this.details = details;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 
 }
--- src/main/java/lt/akademija/javatech/ProductEntity.java
+++ src/main/java/lt/akademija/javatech/ProductEntity.java
@@ -1,7 +1,6 @@
 package lt.akademija.javatech;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,50 +9,53 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class ProductEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	// ex02
-	/*
-	 * cascade padaro, kad visu tipu cia daromi pakeitimai atsispindetu
-	 * ir vaikiniame Entity. Vietoj ALL gali buti ir kiti tipai - MERGE,
-	 * PERSIST, ir t.t.
-	 * 
-	 * vienpusis sarysis is produkto i detales.
-	 * ProductEntity lenteleje sukuria nauja stulpeli DETAILS_ID - 
-	 * 
-	 * TODO neatsiranda nuoroda i ProductDetailsEntity.
-	 */
-	@OneToOne(cascade = CascadeType.ALL)
-	private ProductDetailsEntity details;
+    private String title;
 
-	@Column
-	private String title;
+    // ex02
+    // Uni-directional One to One
+    /*
+     * cascade padaro, kad visu tipu cia daromi pakeitimai atsispindetu ir
+     * vaikiniame Entity. Vietoj ALL gali buti ir kiti tipai - MERGE, PERSIST,
+     * ir t.t.
+     * 
+     * vienpusis sarysis is produkto i detales. ProductEntity lenteleje sukuria
+     * nauja stulpeli DETAILS_ID - jame atsiranda nuoroda i ProductDetailsEntity
+     * ID.
+     */
+    /* 
+     * TIP - pildant JSON, stulpelio pavadinimas turi sutapti ne su situo pavadinimu
+     * (pDetails), o turbut su Setteriu/Getteriu parametru pavadinimais. 
+     */
+    @OneToOne(cascade = CascadeType.ALL)
+    private ProductDetailsEntity pDetails;
 
-	// Getters and Setters
-	public Long getId() {
-		return id;
-	}
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public ProductDetailsEntity getDetails() {
-		return details;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setDetails(ProductDetailsEntity details) {
-		this.details = details;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public ProductDetailsEntity getDetails() {
+        return pDetails;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setDetails(ProductDetailsEntity details) {
+        this.pDetails = details;
+    }
 
 }

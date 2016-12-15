b6b996da997f68c68bcb0bf536ffdc6c61c6e4c9
matas-g <matas.guzelis@gmail.com>
2016-12-15 09:57:56 +0200

    ex02 update

--- src/main/java/lt/akademija/javatech/entity/ProductDetailsEntity.java
+++ src/main/java/lt/akademija/javatech/entity/ProductDetailsEntity.java
@@ -17,64 +17,63 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 @Table(name = "PRODUCT_DETAILS")
 public class ProductDetailsEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ID")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
 
-    @Column(name = "DETAILS")
-    private String details;
+	@Column(name = "DETAILS")
+	private String details;
 
-    // Bi-directional One to One
-    /*
-     * 1. I vaikine Entity pridedam tevines Entity objekta
-     * 
-     * !!! Pridejus jo getters ir setters prasideda Stack Overflow !!! - turbut
-     * to galima isvengt papildant metodus Controller/Service/Repository.
-     * 
-     * 2. Pridedam anotacija OneToOne su "mappedBy".
-     * 
-     * mappedBy reikalinga tik dvipusiam (Bi-directional) rysiui.
-     * 
-     * Prie mappedBy turi buti nurodytas tevineje Entity klaseje sukurto
-     * ProductDetails objekto pavadinimas.
-     */
+	// Bi-directional One to One
+	/*
+	 * 1. I vaikine Entity pridedam tevines Entity objekta
+	 * 
+	 * !!! Pridejus jo getters ir setters prasideda Stack Overflow !!! - turbut
+	 * to galima isvengt papildant metodus Controller/Service/Repository.
+	 * 
+	 * 2. Pridedam anotacija OneToOne su "mappedBy".
+	 * 
+	 * mappedBy reikalinga tik dvipusiam (Bi-directional) rysiui.
+	 * 
+	 * Prie mappedBy turi buti nurodytas tevineje Entity klaseje sukurto
+	 * ProductDetails objekto pavadinimas.
+	 */
 
-    /*
-     * TODO - kaip suprast?:
-     * "Using mappedBy, If we only call person.getDocuments().add(document),
-     * the foreign key in ID_DOCUMENTS will NOT be linked to the new document,
-     * because this is not the owning /tracked side of the relation!
-     * 
-     * To link the document to the new person, you need to explicitly call
-     * document.setPerson(person), because that is the owning side of the relation.
-     * (IdDocument - owning entity)"
-     */
-    
-    // TIP: stulpelis neatvaizduojamas kai naudojama mappedBy.
-    // @OneToOne(mappedBy = "productDetails", cascade = CascadeType.ALL)
-    
-    // Andriaus Sareika 1-N N-1 bi-directional sprendimas (anotacijos)
-    // jis naudojo ir setters / getters - ar jie tikrai cia reikalingi?
-    @OneToOne(cascade = CascadeType.ALL)
-    @JsonBackReference
-    private ProductEntity product;
+	/*
+	 * TODO - kaip suprast?: "Using mappedBy, If we only call
+	 * person.getDocuments().add(document), the foreign key in ID_DOCUMENTS will
+	 * NOT be linked to the new document, because this is not the owning
+	 * /tracked side of the relation!
+	 * 
+	 * To link the document to the new person, you need to explicitly call
+	 * document.setPerson(person), because that is the owning side of the
+	 * relation. (IdDocument - owning entity)"
+	 */
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
+	// Andriaus Sareika 1-N N-1 bi-directional sprendimas (anotacijos)
+	// jis naudojo ir setters / getters - ar jie tikrai cia reikalingi?
+	// @OneToOne(cascade = CascadeType.ALL)
+	// @JsonBackReference
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	@OneToOne(mappedBy = "productDetails", cascade = CascadeType.ALL)
+	private ProductEntity product;
 
-    public String getDetails() {
-        return details;
-    }
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
 
-    public void setDetails(String details) {
-        this.details = details;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
 
 }
